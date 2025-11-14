89fc7ac91520651ae75fedff04bdebb4a72f6631
griffotech <griffo.tech@gmail.com>
2025-11-14 20:40:07 +0300

    Fix test_has_license method signature to be on single line

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -63,12 +63,8 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
-    def test_has_license(
-        self,
-        repo: Dict,
-        license_key: str,
-        expected: bool
-    ) -> None:
+    def test_has_license(self, repo: Dict, license_key: str,
+                         expected: bool) -> None:
         """Test has_license returns True/False based on license key."""
         self.assertEqual(
             GithubOrgClient.has_license(repo, license_key),
